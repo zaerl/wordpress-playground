a10dfc96d07961fafc1ebfc48b65c790a1d720b4
Bero <berislav.grgicak@gmail.com>
2024-11-14 09:56:02 +0100

    Track errors with unknown source

--- packages/playground/website/src/components/layout/index.tsx
+++ packages/playground/website/src/components/layout/index.tsx
@@ -160,9 +160,7 @@ function Modals(blueprint: Blueprint) {
 	useEffect(() => {
 		addCrashListener(logger, (e) => {
 			const error = e as CustomEvent;
-			if (error.detail?.source) {
-				logErrorEvent(error.detail.source);
-			}
+			logErrorEvent(error.detail?.source ?? 'unknown');
 
 			if (error.detail?.source === 'php-wasm') {
 				dispatch(setActiveModal(modalSlugs.ERROR_REPORT));
